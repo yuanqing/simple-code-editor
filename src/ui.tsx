52bc994ddc396daffa85a98600b701b997ae109c
yuanqing <desk@yuanqing.sg>
2021-09-22 07:34:05 +0200

    Set up `prismjs`, bump to `1.5.0-alpha.1`

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,25 +1,27 @@
 import { render } from '@create-figma-plugin/ui'
 import { h } from 'preact'
 import { useState } from 'preact/hooks'
+import { highlight, languages } from 'prismjs'
 import Editor from 'react-simple-code-editor'
 
+import 'prismjs/components/prism-clike.js'
+import 'prismjs/components/prism-javascript.js'
+import '!prismjs/themes/prism.css'
+
 function Plugin() {
-  const [code, setCode] = useState(
-    `function add(a, b) {\n  return a + b;\n}`
-  )
-  function highlight(code: string) {
-    return code
-  }
+  const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`)
   return (
     <Editor
-      value={code}
+      highlight={function (code: string) {
+        return highlight(code, languages.js, 'js')
+      }}
       onValueChange={setCode}
-      highlight={highlight}
       padding={10}
       style={{
         fontFamily: '"Fira code", "Fira Mono", monospace',
         fontSize: 12
       }}
+      value={code}
     />
   )
 }
